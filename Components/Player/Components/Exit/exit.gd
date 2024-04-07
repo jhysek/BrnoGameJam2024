cfb10aaf221e2e4521dd365805dabdb7003ba516
Jiri Hysek <jiri.hysek@gmail.com>
2024-04-07 14:18:02 +0200

    WTF? Deleted some garbage

--- Components/Player/Components/Exit/exit.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite2D
-
-signal exit_reached
-
-func _on_area_2d_body_entered(body):
-	if body.is_in_group("Player"):
-		emit_signal("exit_reached")
--- Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Components/Player/Scenes/menu.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	Transition.openScene()
-
-func _on_button_pressed():
-	Transition.switchTo("res://Scenes/Game.tscn")
